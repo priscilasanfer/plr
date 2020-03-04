a1a83d4a426dbaba7607116f8c08164ec344bdf6
Fonseca, Priscila Santos Ferreira <priscila.fonseca@wirecard.com>
2020-03-04 11:38:53 -0300

    iniciando o projeto

---
+++ src/main/java/io/lfr/plr/PlrApplication.java
@@ -0,0 +1,13 @@
+package io.lfr.plr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lfr/plr/PlrApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lfr.plr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
